986d6892d4cc016553d5f5fe683c9708453f4c9a
RheaBubbles <augbubbles@foxmail.com>
2018-07-10 18:31:41 +0800

    Add the SMS app

---
+++ SMSApp/app/src/main/java/com/hit/bubbl/smsapp/MainActivity.java
@@ -0,0 +1,104 @@
+package com.hit.bubbl.smsapp;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText phoneNumber, smsText;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        phoneNumber = findViewById(R.id.phone_number);
+        smsText = findViewById(R.id.sms_text);
+    }
+
+    public void send(View view) {
+        String number = phoneNumber.getText().toString().trim();
+        String text = smsText.getText().toString();
+        if(TextUtils.isEmpty(number) || TextUtils.isEmpty(text)) {
+            Toast.makeText(this, "输入内容不能为空", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("smsto:" + number));
+            intent.putExtra("sms_body", text);
+            startActivity(intent);
+        }
+    }
+
+    public void sendDirect(View view) {
+        String number = phoneNumber.getText().toString().trim();
+        String text = smsText.getText().toString();
+        if(TextUtils.isEmpty(number) || TextUtils.isEmpty(text)) {
+            Toast.makeText(this, "输入内容不能为空", Toast.LENGTH_SHORT).show();
+        } else {
+            // 检查是否获得了权限（Android6.0运行时权限）
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+                // 没有获得授权，申请授权
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                        Manifest.permission.SEND_SMS)) {
+                    // 返回值：
+                    //  如果app之前请求过该权限,被用户拒绝, 这个方法就会返回true.
+                    //  如果用户之前拒绝权限的时候勾选了对话框中”Don’t ask again”的选项,那么这个方法会返回false.
+                    //  如果设备策略禁止应用拥有这条权限, 这个方法也返回false.
+                    //  弹窗需要解释为何需要该权限，再次请求授权
+                    Toast.makeText(MainActivity.this, "请授权！", Toast.LENGTH_LONG).show();
+
+                    // 帮跳转到该应用的设置界面，让用户手动授权
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                } else {
+                    // 不需要解释为何需要该权限，直接请求授权
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.SEND_SMS},
+                            MY_PERMISSIONS_REQUEST_SEND_SMS);
+                }
+            } else {
+                // 已经获得授权，可以发送短信
+                //获取短信管理器
+                SmsManager smsManager = SmsManager.getDefault();
+
+                String SENT_SMS_ACTION = "SENT_SMS_ACTION";
+                Intent sentIntent = new Intent(SENT_SMS_ACTION);
+                PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, sentIntent,
+                        0);
+
+                //处理返回的接收状态
+                String DELIVERED_SMS_ACTION = "DELIVERED_SMS_ACTION";
+                // create the deilverIntent parameter
+                Intent deliverIntent = new Intent(DELIVERED_SMS_ACTION);
+                PendingIntent deliverPI = PendingIntent.getBroadcast(this, 0,
+                        deliverIntent, 0);
+
+                //拆分短信内容（手机短信长度限制）
+                List<String> divideContents = smsManager.divideMessage(text);
+                for (String dText : divideContents) {
+                    smsManager.sendTextMessage(number, null, dText, sentPI, deliverPI);
+                }
+            }
+        }
+    }
+}
